d444158774ad0cfaac7e8f304c980bd1d6c99e58
hassan509 <79840263+hassan509@users.noreply.github.com>
2024-10-28 07:08:55 +0000

    Conversion of dataTypes

---
+++ 01_Basics/03_conersionOperations.js
@@ -0,0 +1,20 @@
+// let score="45abc";
+// console.log(score);
+// console.log(typeof (score));
+
+// let inNumber=Number(score);
+// console.log(typeof (inNumber));
+// console.log(inNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false=> 0
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof (booleanIsLoggedIn));
+
+// 1=> true, 0 => false
+// "" => false
+// "hassan" => true
